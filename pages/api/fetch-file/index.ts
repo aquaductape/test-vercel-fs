74b2768a7021cef3718542de91cedad99f61093b
Caleb Taylor <aquaductape@gmail.com>
2022-07-06 16:13:09 -0700

    update

--- pages/api/fetch-file/index.ts
+++ pages/api/fetch-file/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 const handler = async (req, res) => {
   if (req.method.toUpperCase() === "POST") {
     try {
-      const file = path.join(process.cwd(), "files", "email.html");
+      const file = path.join(process.cwd(), "files/email.html");
       const html = fs.readFileSync(file).toString();
 
       return res.status(200).json({ msg: "Success!!!", data: html });
